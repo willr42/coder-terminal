3486271d0e6172d0581fd5f0bc00bb45097c141a
Will Robertson <williamdrobertson42@gmail.com>
2022-12-11 11:30:21 +1100

    Add Library test for default args

--- src/tests/test_Library.py
+++ src/tests/test_Library.py
@@ -1,13 +1,21 @@
 from ..Library import Library
+import platformdirs
 
 
 class TestLibrary:
     def test_can_find_file_correctly(self, tmp_path):
-        print(self)
         """Creates temp env with existing file and checks function correctly returns path."""
         temp_directory = tmp_path / "greatreads"
         temp_directory.mkdir()
         temp_file = temp_directory / "library.json"
         temp_file.touch()
-        result = Library.initialise_library_file(tmp_path, temp_file)
+        result = Library(tmp_path, temp_file)
         assert result
+
+    def test_default_filepath(self):
+        """Test if Library creates default file without any arguments."""
+        result = Library()
+        assert (
+            result.json_path
+            == platformdirs.user_data_path() / "greatreads" / "library.json"
+        )
