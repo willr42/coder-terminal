eb2fbce253e6b0806d936c044da4d51e6ee7c875
Will Robertson <williamdrobertson42@gmail.com>
2022-12-12 18:07:01 +1100

    Create ShelfList widget to show all shelves

--- src/views/shelf_view.py
+++ src/views/shelf_view.py
@@ -14,3 +14,17 @@ class ShelfView(npyscreen.Form):
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
+
+
+class ShelfList(npyscreen.MultiLineAction):
+    def __init__(self, *args, **keywords):
+        super(ShelfList, self).__init__(*args, **keywords)
+
+    # change how each shelf is displayed
+    def display_value(self, vl):
+        print(self)
+        return vl.shelf_name.title()
+
+    # change the action on keypress
+    def actionHighlighted(self, act_on_this, key_press):
+        print(key_press)
