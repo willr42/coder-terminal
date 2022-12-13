63df16f4c8fd1db1b2d4472ab549331e6e0af997
Will Robertson <williamdrobertson42@gmail.com>
2022-12-13 20:20:58 +1100

    Add update_book method

--- src/Book.py
+++ src/Book.py
@@ -3,3 +3,6 @@ class Book:
         self.title = data.get("title", None)
         self.author = data.get("author", None)
         self.first_publish_year = data.get("first_publish_year", None)
+
+    def update_book(self, key_to_update, value_to_change):
+        setattr(self, key_to_update, value_to_change)
