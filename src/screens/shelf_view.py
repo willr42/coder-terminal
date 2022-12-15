76d50575a624e67553d2233fbef8c02525200724
Will Robertson <williamdrobertson42@gmail.com>
2022-12-15 15:58:06 +1100

    Building bookshelf view

--- src/screens/shelf_view.py
+++ src/screens/shelf_view.py
@@ -1,12 +1,18 @@
 from textual.screen import Screen
+from textual.containers import Vertical, Container
 from textual.widgets import ListView, ListItem, Label, Header, Footer
 
 
 class ShelfView(Screen):
     def compose(self):
         yield Header()
+        yield Footer()
         list_items = []
         for shelf in self.app.library.contents:
             list_items.append(ListItem(Label(f"{shelf.shelf_name}")))
-        yield ListView(*list_items)
-        yield Footer()
+
+        yield Label("Your Bookshelves", id="shelfTitle")
+        yield Container(
+            Vertical(ListView(*list_items), id="shelfListContainer"),
+            id="shelfViewContainer",
+        )
