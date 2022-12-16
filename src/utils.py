8ea09379ae8cd63c9206a4589045fe6ff984ac6c
Will Robertson <williamdrobertson42@gmail.com>
2022-12-17 10:12:29 +1100

    Remove accidentally-included comment scaffold, docstring

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,17 @@
 from rich.table import Table
 
-# console.print("SHELF MENU", style="b u")
-# table = Table(show_header=False, box=False)
-# table.add_column("Option", style="bold green")
-# table.add_row("V", "View Books on Shelf")
-# table.add_row("A", "Add New Shelf")
-# table.add_row("E", "Edit Shelf Name")
-# table.add_row("R", "Remove Shelf")
-# table.add_row("Q", "Quit")
 
-# console.print(table)
 def create_menu_table(table_key, **table_options):
+    """Creates a table menu, and passes all other options through to Table constructor.
+
+    Args:
+        table_key (dict): key and description
+        **table_options: kwargs accepted by `rich.table.Table` class.
+
+    Returns:
+        Table: Table ready to be printed by `rich.console.Console`.
+    """
+
     table = Table(**table_options)
     table.add_column("Option", style="bold green")
     for key, value in table_key.items():
