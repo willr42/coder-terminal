f52447d162297fa89b595cf222c8f6c54393f32b
Will Robertson <williamdrobertson42@gmail.com>
2022-12-17 22:32:11 +1100

    Add sorting

--- src/menus/shelf_detail_menu.py
+++ src/menus/shelf_detail_menu.py
@@ -16,6 +16,7 @@ def shelf_detail_menu(library, active_shelf_name):
         "A": menu_add_book,
         "E": menu_edit_book,
         "R": menu_delete_book,
+        "S": menu_sort_shelf,
     }
 
     console = Console()
@@ -55,6 +56,7 @@ def print_shelf_detail_menu(active_shelf, console):
         "A": "Add a Book",
         "E": "Edit a Book",
         "R": "Remove a Book",
+        "S": "Sort Shelf",
         "B": "Go Back",
         "Q": "Quit",
     }
@@ -108,6 +110,30 @@ def menu_delete_book(active_shelf, console):
             console.print(f"Sorry, I can't find that book.", style="red b")
             return
         active_shelf.remove_book(book)
+
+
+def menu_sort_shelf(active_shelf, console):
+    """Prints a menu and sorts depending on user input."""
+    print_sort_menu(console)
+    user_choice = menu_option_input("Sort by what?")
+    if user_choice == "T":
+        active_shelf.contents.sort(key=lambda book: book.title)
+    if user_choice == "A":
+        active_shelf.contents.sort(key=lambda book: book.author)
+    if user_choice == "Y":
+        active_shelf.contents.sort(
+            key=lambda book: book.first_publish_year, reverse=True
+        )
+
+
+def print_sort_menu(console):
+    """Prints the sort menu."""
+    sort_options = {
+        "T": "Sort by Title",
+        "A": "Sort by Author",
+        "Y": "Sort by First Published",
+    }
+    console.print(create_menu_table(sort_options, show_header=False, box=False))
 
 
 def find_book_in_shelf(active_shelf):
