c98cda0cbf6cabb996d2d691f9103e03a3f3381c
Will Robertson <williamdrobertson42@gmail.com>
2022-12-17 15:01:33 +1100

    Refactor input functions to be more specialised

--- src/user_input.py
+++ src/user_input.py
@@ -2,11 +2,36 @@ from rich.console import Console
 from exceptions import UserExited, UserInputEmptyException
 
 
-def handle_user_input(prompt_str):
+def handle_string_input(prompt_str):
     console = Console()
     while True:
         try:
             user_input = collect_user_input(prompt_str)
+        except UserInputEmptyException:
+            console.print("❌ Sorry, your input was empty. Try again.")
+        else:
+            return user_input
+
+
+def handle_int_input(prompt_str):
+    console = Console()
+    while True:
+        try:
+            user_input = collect_user_input(prompt_str)
+            int(user_input)
+        except ValueError:
+            console.print("❌ Sorry, your input wasn't a valid number. Try again.")
+        except UserInputEmptyException:
+            console.print("❌ Sorry, your input was empty. Try again.")
+        else:
+            return int(user_input)
+
+
+def menu_option_input(prompt_str):
+    console = Console()
+    while True:
+        try:
+            user_input = collect_user_input(prompt_str).upper()
         except UserInputEmptyException:
             console.print("❌ Sorry, your input was empty. Try again.")
         else:
